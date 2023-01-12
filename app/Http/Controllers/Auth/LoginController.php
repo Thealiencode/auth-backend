d01bb9dbf52fc8302fdc95a2181ce8ca7d14512e
Golden Moses <macbookair2020m1@MacBook-Air.local>
2023-01-12 15:05:30 +0100

    initial commit

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\LoginRequest;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function login(LoginRequest $request){
+
+        if (auth()->attempt($request->validated)) {
+            $token = auth()->user()->createToken('authToken')->plainTextToken;
+            return response()->json([
+                'status'    => 'success',
+                'token' => $token,
+                'message' => 'SuccessFUlly Logged In',
+                'data' => auth()->guard('web')->user()], 200);
+        } else {
+            return response()->json(['status' => 'error',  'message' => 'Incorrect Login Details', 'status' => 'error'], 401);
+        }
+
+
+    }
+}
---
+++ app/Http/Controllers/Auth/RegistrationController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\RegistrationRequest;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Auth\Events\Registered;
+
+class RegistrationController extends Controller
+{
+    public function register(RegistrationRequest $request){
+
+        $user = User::create([
+            'firstname'     => $request->firstname,
+            'lastname'     => $request->lastname,
+            'email'         => $request->email,
+            'password'      => Hash::make($request->password),
+
+        ]);
+        if($user){
+            event(new Registered($user));
+
+
+            return response()->json(['status' => 'success','message' => 'Account Created Successfully check your mail to verify your account'], 200);
+        } else {
+            return response()->json(['status' => 'error','message' => 'Error While creating Account'], 401);
+
+        }
+    }
+}
